fc27e30ce300541ef3652eea15f3452778b67ec0
bagasditt <adityabagast@gmail.com>
2019-12-18 06:56:48 +0700

    edit repo

---
+++ jsControls/aritmatika.js
@@ -0,0 +1,16 @@
+
+const a = 10;
+const b = 12;
+
+console.log("penjumlahan", a + b);
+
+console.log("pengurangan", a - b);
+
+console.log("perkalian", a * b);
+
+console.log("pembagian", a / b);
+console.log("hasil pembagian", a % b);
+
+
+
+
