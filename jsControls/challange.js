515b71794e7f00a62bf1c4160059312163913282
bagasditt <adityabagast@gmail.com>
2019-12-18 18:28:06 +0700

    tambah data

--- jsControls/challange.js
+++ jsControls/challange.js
@@ -6,3 +6,17 @@ for (let baris = 7; baris > 0; baris--) {
     console.log(hasil);
     
 }
+
+for (let i=1;i<=5;i++){
+    let hasilp = '';
+    for (let j=i;j<=5;j++){
+        hasilp = hasilp.toString().concat(' ')
+    }
+    for (var k=1;k<=i;k++){
+        hasilp = hasilp.toString().concat('*')
+    }
+    for (var l=1;l<=i-1;l++){
+        hasilp = hasilp.toString().concat('*')
+    }
+    console.log(hasilp)
+}
